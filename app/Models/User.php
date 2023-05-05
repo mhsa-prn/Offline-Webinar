6aa0e1a03a98da392f460014d509da156be3d99c
Mahsa Piran <piran_mahsa@yahoo.com>
2023-05-05 20:09:16 +0430

    Email implementation, Code table
    Email Verification, Password recovery by email, TOTP implementation (table and relations)

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -22,6 +23,11 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+
+    public function code()
+    {
+        return $this->hasOne(Code::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
