d2b7a7202d6820cd245c48a1f8638237fd495ccc
Mahsa Piran <piran_mahsa@yahoo.com>
2023-05-30 16:01:32 +0330

    categories implementation

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index(Request $request)
+    {
+        $categories = Category::latest()->paginate(10);
+        return view('admin.categories.index',compact('categories'));
+    }
+}
