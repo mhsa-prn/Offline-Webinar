00f6c0455b32e99fc63269584c5fcd256d9637bc
Mahsa Piran <piran_mahsa@yahoo.com>
2023-06-12 17:52:24 +0330

    create index page / show webinars

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Webinar;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,5 +25,11 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function firstPage()
+    {
+        $webinars= Webinar::where('confirmed',1)->latest()->paginate(10);
+        return view('welcome',compact('webinars'));
     }
 }
